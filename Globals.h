90a7edfa00855e9bbcbabafe53cf0194b958e085
Miika Pakarinen <miika.pakarinen@gmail.com>
2013-01-13 20:59:26 +0200

    Module 3 commit v2
    Fixed globals.h -> Globals-h

---
+++ Globals.h
@@ -0,0 +1,12 @@
+////////////////////
+/// C++ Object-oriented Programming
+/// Adventure Game 6
+/// Reference answer 
+/// anssi.grohn@pkamk.fi
+////////////////////
+#ifndef __Globals_h__
+#define __Globals_h__
+
+enum Direction { North, South, East, West, kNumDirs };
+
+#endif
